454c05c7d55016e56f2cd2ab821f5f47ae4b98d2
bodie wood <bodie1@macbook-air.lan>
2023-06-14 11:38:29 -0700

    Update file to include sections for about me page, instead of divs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 const AboutMePage = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <main>
+      <section>
+        <div>Image</div>
+        <div>Text</div>
+      </section>
+      <section></section>
+      <section>
+        <div>Text</div>
+        <div>Image</div>
+      </section>
+    </main>
   )
 }
 export default AboutMePage
