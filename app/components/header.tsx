c20cc08c56222beebeb3b37edd364897c382c1ec
bodie wood <bodie1@macbook-air.lan>
2023-06-16 12:39:27 -0700

    Update padding, and margins

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,18 +14,18 @@ type HeaderProps = {
 export const Header = ({name, pages}: HeaderProps ) => {
   return (
     <header className='flex flex-col relative bg-linear-557AFF w-full h-48'> 
-     <div className='flex flex-col justify-around h-full px-6 bg-header z-10'> 
-      <nav className='text-right flex flex-row gap-6 self-end'>
+     <div className='flex flex-col justify-around h-full sm:px-6 px-1 bg-header z-10'> 
+      <nav className='text-right flex flex-row sm:gap-6 self-end gap-0'>
         {pages.map((page) => {
           return(
-            <Link aria-label={page.label} key={page.id} href={page.link} className='px-6 py-2 min-w-100 hover:bg-blue-light hover:text-blue font-display uppercase tracking-wide'>
+            <Link aria-label={page.label} key={page.id} href={page.link} className='px-6 py-2 min-w-100 hover:bg-blue-light hover:text-blue font-display uppercase tracking-wide sm:bg-transparent bg-white'>
             {page.label}
             </Link>
           )
         })
         }
       </nav>
-      <h1 className='text-white uppercase'>{name}</h1>
+      <h1 className='text-white uppercase pl-2 sm:pl-0'>{name}</h1>
      </div>
       <Image priority height={400} width={2000} className='w-full h-48 object-cover object-top absolute mb-6' src='/images/header-image.jpeg' alt={`Image of ${name}`} aria-label={`Image of ${name}`} />
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image'
 
 const AboutMePage = () => {
   return (
-    <main className='flex flex-col items-center mb-32'>
-      <section className='flex flex-row w10/12 mt-10 mb-6 items-center'>
-        <div className='w-1/3'>
+    <main className='flex flex-col items-center sm:mb-32 mb-16'>
+      <section className='flex sm:flex-row sm:w-10/12 items-center flex-col w-full'>
+        <div className='sm:w-1/3 w-full flex justify-center mt-10'>
           <Image width={640} height={960} aria-label={'Image of Bodie Wood'} src={'/images/animegan_toon_portfolio-pic.jpeg'} priority={false} className='profileImage'/>
           </div>
-        <div className='ml-20 w-2/3'>
+        <div className='sm:ml-20 sm:mt-0 sm:w-2/3 w-full mt-6 ml-0 p-10'>
           <h2>{aboutMeData.title}</h2>
           <p className='text-base'>{aboutMeData.body}</p>
           </div>
       </section>
-      <section className='w-full my-8 bg-blue-light p-10 h-full'>
+      <section className='w-full bg-blue-light p-10 h-full'>
         <p>{aboutMeData.highlightedBody}</p>
       </section>
-      <section className='flex flex-row w-10/12 mt-10 items-center'>
-        <div className='flex flex-col w-7/12'>
+      <section className='flex sm:flex-row sm:w-10/12 p-10 items-center w-full flex-col'>
+        <div className='flex flex-col w-full mb-10 sm:mb-0 sm:w-7/12'>
           <p>{aboutMeData.body2}</p>
           </div>
-        <div className='w-5/12 ml-10'>
+        <div className='sm:w-5/12 sm:ml-10 w-full ml-0'>
              <Image  width={500} height={250} aria-label={'Image of Bodie Wood'} src={'/images/animegan_toon_portfolio-pic.jpeg'} priority={false} className='h-250 object-cover w-500'/>
         </div>
       </section>
