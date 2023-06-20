5cb783a713e09c77d14f8e71893c23e58406a9d3
bodie wood <bodie1@macbook-air.lan>
2023-06-20 12:43:12 -0700

    Update nav bar location

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -15,7 +15,7 @@ export const Header = ({name, pages}: HeaderProps ) => {
   return (
     <header className='flex flex-col relative bg-linear-557AFF w-full h-48'> 
      <div className='flex flex-col justify-around h-full sm:px-6 px-1 bg-header z-10'> 
-      <nav className='text-right flex flex-row sm:gap-6 self-end gap-0'>
+      <nav className='text-left flex flex-row sm:gap-6 gap-0'>
         {pages.map((page) => {
           return(
             <Link aria-label={page.label} key={page.id} href={page.link} className='px-6 py-2 min-w-100 hover:bg-blue-light hover:text-blue font-display uppercase tracking-wide sm:bg-transparent bg-white'>
