f7376aab5d7ebc441dca677618d25470e2bf8fca
bodie wood <bodie1@macbook-air.lan>
2023-08-08 13:09:02 -0700

    Add navbar tabs for different project types.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,17 @@
 'use client';
 import Navbar from '../components/navbar';
 import { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faSquareJs, faGit, faGitlab, faNode, faBootstrap, faPhp } from '@fortawesome/free-brands-svg-icons';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Carousel } from 'flowbite-react';
 import '@/styles/skills.css';
 
 const Projects = () => {
-  const [containerHeight, setContainerHeight] = useState(0);
   const [fadeIn, setFadeIn] = useState(false);
-  const [contain, setContain] = useState('cover');
   const [viewportWidth, setViewportWidth] = useState(0);
+  const [featuredProjects, setFeaturedProjects] = useState(true);
+  const [reactProjects, setReactProjects] = useState(false);
+  const [cSharpProjects, setCSharpProjects] = useState(false);
+  const [qaProjects, setQAProjects] = useState(false);
 
   useEffect(() => {
     setFadeIn(true);
@@ -36,6 +34,41 @@ const Projects = () => {
     };
   }, []);
 
+  const showProjects = (project: string) => {
+    switch (project) {
+      case 'featured':
+        setFeaturedProjects(true);
+        setReactProjects(false);
+        setCSharpProjects(false);
+        setQAProjects(false);
+        break;
+      case 'react':
+        setFeaturedProjects(false);
+        setReactProjects(true);
+        setCSharpProjects(false);
+        setQAProjects(false);
+        break;
+      case 'csharp':
+        setFeaturedProjects(false);
+        setReactProjects(false);
+        setCSharpProjects(true);
+        setQAProjects(false);
+        break;
+      case 'qa':
+        setFeaturedProjects(false);
+        setReactProjects(false);
+        setCSharpProjects(false);
+        setQAProjects(true);
+        break;
+      default:
+        setFeaturedProjects(true);
+        setReactProjects(false);
+        setCSharpProjects(false);
+        setQAProjects(false);
+        break;
+    }
+  };
+
   return (
     <div
       className='home 2xl:home-large flex flex-col lg:grow w-7/8 p-5 md:py-5 bg-white rounded-3xl md:my-10 md:mx-10 dark:bg-gray-800'
@@ -50,7 +83,7 @@ const Projects = () => {
 
       <div className='sm:hidden'>
         <label htmlFor='tabs' className='sr-only'>
-          Select your country
+          Project Type
         </label>
         <select
           id='tabs'
@@ -63,88 +96,244 @@ const Projects = () => {
         </select>
       </div>
       <ul className='hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400 border'>
-        <li className='w-full'>
+        <li className='w-full' onClick={() => showProjects('featured')}>
           <a
             href='#'
-            className='inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white'
+            className={`${
+              featuredProjects && 'active bg-gray-200 text-gray-900 dark:text-white dark:bg-gray-700'
+            } inline-block w-full p-4 hover:text-gray-700 hover:bg-gray-200 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:hover:bg-gray-700 rounded-l-lg`}
             aria-current='page'
           >
             Featured
           </a>
         </li>
-        <li className='w-full'>
+        <li className='w-full' onClick={() => showProjects('react')}>
           <a
             href='#'
-            className='inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
+            className={`${
+              reactProjects && 'active bg-gray-200 text-gray-900 dark:text-white dark:bg-gray-700'
+            } inline-block w-full p-4 hover:text-gray-700 hover:bg-gray-200 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:hover:bg-gray-700`}
           >
             React
           </a>
         </li>
-        <li className='w-full'>
+        <li className='w-full' onClick={() => showProjects('csharp')}>
           <a
             href='#'
-            className='inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
+            className={`${
+              cSharpProjects && 'active bg-gray-200 text-gray-900 dark:text-white dark:bg-gray-700'
+            } inline-block w-full p-4 hover:text-gray-700 hover:bg-gray-200 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:hover:bg-gray-700`}
           >
             C# .NET
           </a>
         </li>
-        <li className='w-full'>
+        <li className='w-full' onClick={() => showProjects('qa')}>
           <a
             href='#'
-            className='inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
+            className={`${
+              qaProjects && 'active bg-gray-200 text-gray-900 dark:text-white dark:bg-gray-700'
+            } inline-block w-full p-4 rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:hover:bg-gray-700`}
           >
             QA
           </a>
         </li>
       </ul>
-
-      <div className='flex flex-col md:flex-row w-full h-full p-4'>
-        <div className='flex flex-col md:w-1/2 h-full'>
-          <div className='grid grid-cols-1 h-full space-y-4'>
-            <div className='relative'>
-              <Image
-                className='rounded-lg hover:blur-sm cursor-pointer'
-                src={'/images/gear-up-landing-page.png'}
-                alt='image'
-                fill
-                sizes='100vw'
-                style={{
-                  objectFit: 'cover',
-                }}
-              />
+      {featuredProjects && (
+        <div className='flex flex-col md:flex-row w-full h-full p-4'>
+          <div className='flex flex-col md:w-1/2 h-full'>
+            <div className='grid grid-cols-1 h-full space-y-4'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/gear-up-landing-page.png'}
+                  alt='image'
+                  fill
+                  sizes='100vw'
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/quoted-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
             </div>
-            <div className='relative'>
-              <Image
-                className='rounded-lg hover:blur-sm cursor-pointer'
-                src={'/images/quoted-landing-page.png'}
-                alt='image'
-                sizes='100vw'
-                fill
-                style={{
-                  objectFit: 'cover',
-                }}
-              />
+          </div>
+          <div className='flex flex-col md:flex-row md:w-1/2 h-full pt-4 md:pt-0 md:pl-4 md:pl-0'>
+            <div className='grid grid-cols-1 h-1/2 md:h-full w-full'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/afterwise-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                    objectPosition: 'top',
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className='flex flex-col md:flex-row md:w-1/2 h-full pt-4 md:pt-0 md:pl-4 md:pl-0'>
-          <div className='grid grid-cols-1 h-1/2 md:h-full w-full'>
-            <div className='relative'>
-              <Image
-                className='rounded-lg hover:blur-sm cursor-pointer'
-                src={'/images/afterwise-landing-page.png'}
-                alt='image'
-                sizes='100vw'
-                fill
-                style={{
-                  objectFit: 'cover',
-                  objectPosition: 'top',
-                }}
-              />
+      )}
+      {reactProjects && (
+        <div className='flex flex-col md:flex-row w-full h-full p-4'>
+          <div className='flex flex-col md:w-1/2 h-full'>
+            <div className='grid grid-cols-1 h-full space-y-4'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/backroads-landing-page.png'}
+                  alt='image'
+                  fill
+                  sizes='100vw'
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/quoted-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row md:w-1/2 h-full pt-4 md:pt-0 md:pl-4 md:pl-0'>
+            <div className='grid grid-cols-1 h-1/2 md:h-full w-full'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/gear-up-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                    objectPosition: 'top',
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+      {cSharpProjects && (
+        <div className='flex flex-col md:flex-row w-full h-full p-4'>
+          <div className='flex flex-col md:w-1/2 h-full'>
+            <div className='grid grid-cols-1 h-full space-y-4'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/gear-up-landing-page.png'}
+                  alt='image'
+                  fill
+                  sizes='100vw'
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/quoted-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row md:w-1/2 h-full pt-4 md:pt-0 md:pl-4 md:pl-0'>
+            <div className='grid grid-cols-1 h-1/2 md:h-full w-full'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/afterwise-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                    objectPosition: 'top',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {qaProjects && (
+        <div className='flex flex-col md:flex-row w-full h-full p-4'>
+          <div className='flex flex-col md:w-1/2 h-full'>
+            <div className='grid grid-cols-1 h-full space-y-4'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/gear-up-landing-page.png'}
+                  alt='image'
+                  fill
+                  sizes='100vw'
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/quoted-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row md:w-1/2 h-full pt-4 md:pt-0 md:pl-4 md:pl-0'>
+            <div className='grid grid-cols-1 h-1/2 md:h-full w-full'>
+              <div className='relative'>
+                <Image
+                  className='rounded-lg hover:blur-sm cursor-pointer'
+                  src={'/images/afterwise-landing-page.png'}
+                  alt='image'
+                  sizes='100vw'
+                  fill
+                  style={{
+                    objectFit: 'cover',
+                    objectPosition: 'top',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
